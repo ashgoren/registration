50c6d62874c688b95b1f44dc72100cb9483a5323
Ash Goren <ash@mortalwombat.net>
2025-06-20 01:02:19 -0700

    cleanup setting config

--- update-github-secrets.sh
+++ update-github-secrets.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Read each line in the .env file
-while IFS='=' read -r key value; do
+while IFS='=' read -r key value || [ -n "$key" ]; do
     # Trim whitespace from the key and value
     key=$(echo "$key" | xargs)
     value=$(echo "$value" | xargs)
 
+    # Skip blank lines and comments
+    [[ -z "$key" || "$key" == \#* ]] && continue
+
     # Skip the VITE_USE_FIREBASE_EMULATOR key
-    if [ "$key" == "VITE_USE_FIREBASE_EMULATOR" ]; then
-        continue
-    fi
+    [[ "$key" == "VITE_USE_FIREBASE_EMULATOR" ]] && continue
 
     # Set each key-value pair as a GitHub secret
     echo "Setting secret for $key"
