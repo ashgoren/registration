2c224c4ec33e8ef3b03d444e4d6135cbba56c591
Ash Goren <ash@mortalwombat.net>
2025-06-29 12:53:11 -0700

    refactor & add retries to webhooks

--- functions/shared/orders.js
+++ functions/shared/orders.js
@@ -1,9 +1,9 @@
 import { getFirestore } from 'firebase-admin/firestore';
 import { createError, ErrorType } from '../shared/errorhandler.js';
 
-const ordersCollection = getFirestore().collection('orders');
+export const ordersCollection = getFirestore().collection('orders');
 
-const getOrders = async ({ pending = false, testMode = 'all' } = {}) => {
+export const getOrders = async ({ pending = false, testMode = 'all' } = {}) => {
   let query = ordersCollection.where('status', '==', pending ? 'pending' : 'final').orderBy('createdAt', 'asc');
   if (testMode !== 'all') {
     query = query.where('isTestOrder', '==', testMode);
@@ -19,27 +19,9 @@ const getOrders = async ({ pending = false, testMode = 'all' } = {}) => {
   }
 };
 
-const getOrderByPaymentId = async (paymentId, testMode) => {
-  try {
-    const snapshot = await ordersCollection
-      .where('paymentId', '==', paymentId)
-      .where('isTestOrder', '==', testMode)
-      .get();
-    if (snapshot.empty) {
-      return null;
-    }
-    const order = snapshot.docs[0].data();
-    return { key: snapshot.docs[0].id, ...order };
-  } catch (err) {
-    throw createError(ErrorType.DATABASE_READ, `Error reading order with paymentId ${paymentId}: ${err.message}`);
-  }
-};
-
-const getPendingOrdersMissingFromFinalOrders = async () => {
+export const getPendingOrdersMissingFromFinalOrders = async () => {
   const pendingOrders = await getOrders({ pending: true });
   const finalOrders = await getOrders({ pending: false });
   const finalOrderEmails = new Set(finalOrders.map(order => order.people[0].email));
   return pendingOrders.filter(order => !finalOrderEmails.has(order.people[0].email));
 };
-
-export { ordersCollection, getOrders, getOrderByPaymentId, getPendingOrdersMissingFromFinalOrders };
