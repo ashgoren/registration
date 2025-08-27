a7f98f28121944a51def6199535dbc04d544c05e
Ash Goren <ash@mortalwombat.net>
2025-08-27 16:07:25 -0700

    add sourcemaps

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true
+  },
   plugins: [
     react(),
     eslint({
