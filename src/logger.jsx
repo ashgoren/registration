aa7e794e4ea37714d5a5e1b1bd017bce2ce11948
Ash Goren <ash@mortalwombat.net>
2025-09-05 14:43:28 -0700

    switch logger from papertrail to google cloud logging

--- src/logger.jsx
+++ src/logger.jsx
@@ -1,40 +1,30 @@
 import { firebaseFunctionDispatcher } from 'src/firebase.jsx';
-import { config } from 'config';
-const { FIREBASE_PROJECT_ID } = config;
 
 export const log = (message, metadata) => logger('info', message, metadata);
 export const logWarn = (message, metadata) => logger('warn', message, metadata);
 export const logError = (message, metadata) => logger('error', message, metadata);
-export const logDivider = () => logger('divider', '-'.repeat(125));
 
-const logger = (level, msg, metadata = {}) => {
-  const isLog = level !== 'divider';
-
+const logger = (level, message, metadata = {}) => {
   // log locally
-  if (isLog) {
-    console[level](msg, ...(Object.keys(metadata).length ? [metadata] : []));
-  }
+  console[level](message, ...(Object.keys(metadata).length ? [metadata] : []));
 
-  // log to Papertrail via Firebase Function (production only)
+  // log to Google Cloud logging via Firebase Function (production only)
   if (import.meta.env.PROD) {
     const { email, order, error, userAgent, ...rest } = metadata;
-    const includeLevel = level === 'warn' || level === 'error';
-    const includeMetadata = Object.keys(rest).length > 0;
 
     const payload = {
-      ...includeLevel ? { level } : {},
-      msg,
-      ...isLog && { timestamp: new Date().toISOString() },
+      level,
+      message,
+      timestamp: new Date().toISOString(),
       ...email && { email },
       ...userAgent && { userAgent },
       ...order && { order },
       ...error && { error },
-      ...includeMetadata && { metadata: rest },
-      ...isLog && { project: FIREBASE_PROJECT_ID }
+      ...(Object.keys(rest).length && { metadata: rest })
     };
 
     firebaseFunctionDispatcher({
-      action: 'logToPapertrail',
+      action: 'logEvent',
       data: payload,
       ...email && { email }
     });
