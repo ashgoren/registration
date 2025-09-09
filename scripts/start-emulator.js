1d707218b07d0a579c6611b5d09034eb7ad0eba0
Ash Goren <ash@mortalwombat.net>
2025-09-09 00:19:34 -0700

    direct firebase emulators to use stg project

--- scripts/start-emulator.js
+++ scripts/start-emulator.js
@@ -4,7 +4,7 @@ import { spawn } from 'child_process';
 
 process.chdir('./functions');
 
-const child = spawn('doppler', ['run', '--', 'firebase', 'emulators:start'], { stdio: 'inherit' });
+const child = spawn('doppler', ['run', '--', 'firebase', 'emulators:start', '-P', 'staging'], { stdio: 'inherit' });
 
 process.on('SIGINT', () => {
   child.kill('SIGINT');
