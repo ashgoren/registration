5ed8177d056aa09290ccf4e190bc5700dbf05622
Ash Goren <ash@mortalwombat.net>
2025-09-17 12:56:27 -0700

    test terraform setup for create/delete firebase project [skip deploy]

--- terraform/firebase.tf
+++ terraform/firebase.tf
@@ -1,15 +1,7 @@
-import {
-  to = google_firebase_project.default
-  id = var.project_id
-}
-
+# Create the Firebase project resource
 resource "google_firebase_project" "default" {
   provider    = google
   project     = var.project_id
-
-  lifecycle {
-    prevent_destroy = true
-  }
 
   depends_on = [google_project_service.apis]
 }
