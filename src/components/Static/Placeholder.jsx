5ae65324578dddd4c2b60fe4b8cf5df4b85fa315
Ash Goren <ash@mortalwombat.net>
2025-10-08 15:43:07 -0700

    update placeholder static page

--- src/components/Static/Placeholder.jsx
+++ src/components/Static/Placeholder.jsx
@@ -1,18 +1,22 @@
 import { MaterialLayout } from 'components/layouts';
 import { Typography } from '@mui/material';
-import { StyledPaper, Paragraph } from 'components/layouts/SharedStyles';
+import { StyledPaper, StyledLink, Paragraph } from 'components/layouts/SharedStyles';
+import { websiteLink } from 'utils';
 import { config } from 'config';
-const { EVENT_TITLE, EVENT_LOCATION, EVENT_DATE } = config;
+const { EVENT_TITLE, EVENT_LOCATION, EVENT_DATE, MORE_INFO_URL } = config;
 
 export const Placeholder = () => {
   return (
     <MaterialLayout>
       <StyledPaper>
-        <Paragraph>Join us at {EVENT_TITLE}!</Paragraph>
+        Join us at <StyledLink to={websiteLink(MORE_INFO_URL)}>{EVENT_TITLE}</StyledLink>!<br />
         <Paragraph>Camp is {EVENT_DATE} at {EVENT_LOCATION}.</Paragraph>
         <Typography variant="h6" sx={{ fontStyle: 'italic' }}>
           Registration will open soon!
         </Typography>
+        <Paragraph>
+          For more info, visit <StyledLink to={websiteLink(MORE_INFO_URL)}>{MORE_INFO_URL}</StyledLink>.
+        </Paragraph>
       </StyledPaper>
     </MaterialLayout>
   );
