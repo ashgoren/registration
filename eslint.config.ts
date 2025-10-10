cc307b7849713968deaebe4669c216970985130d
Ash Goren <ash@mortalwombat.net>
2025-10-10 15:32:31 -0700

    config eslint

---
+++ eslint.config.ts
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  { 
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    linterOptions: { reportUnusedDisableDirectives: "error" }
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: { react: { version: "detect" } },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/display-name": "off",
+      "react/prop-types": "off",
+      "react/no-unescaped-entities": "off",
+    },
+  },
+];
