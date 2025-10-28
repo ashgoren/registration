4a57e6c5a58212c2af31261bbb0ad0d4d613948d
Ash Goren <ash@mortalwombat.net>
2025-10-27 23:46:38 -0700

    add ts to usePaypalPayment

---
+++ src/hooks/usePaypalPayment.tsx
@@ -0,0 +1,49 @@
+import { useRef } from 'react';
+import { logInfo, logError } from 'src/logger';
+import * as api from 'src/firebase';
+import type { Order } from 'types/order';
+
+export const usePaypalPayment = ({ order, id }: {
+  order: Order;
+  id: string; // payment intent id
+}) => {
+	const { email } = order.people[0];
+	const idempotencyKeyRef = useRef(crypto.randomUUID());
+
+	const processPayment = async () => {
+		logInfo('Capturing PayPal payment', { email, order });
+		try {
+			const response = await api.capturePaypalOrder({
+				id,
+				idempotencyKey: idempotencyKeyRef.current,
+				email
+			});
+
+			const {
+				id: paymentId,
+				email: paymentEmail,
+				amount
+			} = response || {};
+
+			if (!paymentId || !amount) {
+				throw new Error('Invalid payment capture response: missing payment ID or amount');
+			}
+
+			logInfo('Payment captured', { email, paymentEmail, paymentId, amount });
+
+			return {
+				paymentId,
+				paymentEmail,
+				amount: Number(amount)
+			};
+
+		} catch (error) {
+			logError('PayPal process payment error', { email, error });
+			throw error; // rethrow from backend or above to be handled by PaypalCheckout component
+		} finally {
+			idempotencyKeyRef.current = crypto.randomUUID(); // reset after success, but also after failure since user may change order
+		}
+	};
+
+  return { processPayment };
+}
