f0a4477f449fef6127197ccb57fdbd505d2030a9
Ash Goren <ash@mortalwombat.net>
2025-12-07 15:32:50 -0600

    bugfix: format fees currency output

--- src/components/PaymentFormFees.tsx
+++ src/components/PaymentFormFees.tsx
@@ -1,5 +1,6 @@
 import { Checkbox, FormControlLabel } from '@mui/material';
 import { StyledPaper } from 'components/layouts/SharedStyles';
+import { formatCurrency } from 'utils/misc';
 
 export const PaymentFormFees = ({ fees, coverFees, setCoverFees }:
   { fees: number; coverFees: boolean; setCoverFees: (value: boolean) => void }
@@ -8,7 +9,7 @@ export const PaymentFormFees = ({ fees, coverFees, setCoverFees }:
     <StyledPaper>
       <FormControlLabel
         control={<Checkbox checked={coverFees} onChange={(e) => setCoverFees(e.target.checked)} />}
-        label={`I would like to add $${fees} to cover the transaction fees.`}
+        label={`I would like to add ${formatCurrency(fees)} to cover the transaction fees.`}
       />
     </StyledPaper>
   )
