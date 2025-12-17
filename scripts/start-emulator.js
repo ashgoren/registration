9a957731170e76aebbe894e839dd6164d0b0cecc
Ash Goren <ash@mortalwombat.net>
2025-12-16 20:55:12 -0800

    prep for conversion of functions to ts

--- scripts/start-emulator.js
+++ scripts/start-emulator.js
@@ -4,7 +4,15 @@ import { spawn } from 'child_process';
 
 process.chdir('./functions');
 
-const child = spawn('doppler', ['run', '--', 'firebase', 'emulators:start', '-P', 'staging'], { stdio: 'inherit' });
+const child = spawn('doppler', [
+  'run', '--',
+  'npx', 'concurrently',
+  '--kill-others',
+  '--names', 'TSC,EMU',
+  '--prefix-colors', 'blue,green',
+  '"tsc --watch"',
+  '"firebase emulators:start -P staging"'
+], { stdio: 'inherit', shell: true });
 
 process.on('SIGINT', () => {
   child.kill('SIGINT');
