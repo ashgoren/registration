74b658dd2e996c513597f380af8e9011efc26219
Ash Goren <ash@mortalwombat.net>
2026-01-10 20:04:12 -0600

    add form filling tests

---
+++ tests/testData.ts
@@ -0,0 +1,12 @@
+export const validData: Record<string, string> = {
+  first: 'Ash',
+  last: 'Zamler',
+  email: 'test@example.com',
+  emailConfirmation: 'test@example.com',
+  phone: '555-555-5555',
+  address: '123 Sesame Place',
+  city: 'Portland',
+  state: 'OR',
+  zip: '97204',
+  agreement: 'yes'
+};
